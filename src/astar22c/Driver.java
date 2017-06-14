49ef63c0f6504c55b085bbebea3b05884dd0aac4
Omri Gideoni <omrig.n7@gmail.com>
2017-06-13 20:32:15 -0700

    Intergrated Fawzan's Interface and Vaughn's conversion method.

--- src/astar22c/Driver.java
+++ src/astar22c/Driver.java
@@ -1,10 +1,89 @@
 package astar22c;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Driver 
 {
+	//Global Vars
+	public static Scanner userScanner = new Scanner(System.in);
+	
+	//Main
 	public static void main(String[] args)
 	{
-		System.out.printf("Hello worl!\n");
+		String userInput = ""; //String to hold user input
+		Scanner fileScanner; //Scanner to scan through the input file
+		
+		System.out.printf("Welcome to the pathfinding program!\n");
+		
+		while(!userInput.equals("exit"))
+		{
+			userInput = userScanner.nextLine().toLowerCase().trim();
+			switch(userInput)
+			{
+				case "help":
+					//Display the available commands...
+					break; 
+
+				case "exit":
+					System.out.printf("Exiting program...\n");
+					break; 
+				
+				default:
+					fileScanner = openInputFile(userInput);
+					if(fileScanner == null)
+					{
+						System.out.printf("File not found...\n\n");
+					}
+					else
+					{
+						char dataArray[][] = dataToArray(fileScanner);
+						//Convert to graph
+						//Run pathfinding simulation
+						//Show the path that was found...
+					}
+					break;
+			}
+		}
+		//END OF PROGRAM
+	} // end main
+	
+	
+	public static Scanner openInputFile(String filename) 
+	{
+		File file = new File(filename);
+		Scanner scanner;
+		
+		try 
+		{
+			scanner = new Scanner(file);
+		} // end try
+		catch (FileNotFoundException fe) 
+		{
+			return null; // array of 0 elements
+		} // end catch
+		
+		return scanner;
+	} // end openInputFile
+	
+	//
+	public static char[][] dataToArray(Scanner fileScanner)
+	{
+	    int numRows = fileScanner.nextInt();
+	    int numColumns = fileScanner.nextInt();
+	    char[][] dataArray = new char[numRows][numColumns];
+
+	    for(int row = 0; row < numRows; row++)
+	    {
+	        String line = fileScanner.next();
+
+	        for(int column = 0; column < numColumns; column++)
+	        {
+	        	dataArray[row][column] = line.charAt(column);
+	        }
+	    }
+	    return dataArray;
 	}
 	
 	public enum TileType
